d6246673832d1593a7ec7725fb39b0de3ae6e879
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-02 23:46:08 +0100

    add route protection

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const { login, dashboard } = require('../controllers/index')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 const router = express.Router()
 
 // @desc    Login/Landing page
 // @route   GET /
-router.route('/').get(login)
+router.route('/').get(ensureGuest, login)
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.route('/dashboard').get(dashboard)
+router.route('/dashboard').get(ensureAuth, dashboard)
 
 
 
