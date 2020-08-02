7b706d47f59b5235e7821d1502104ec6c3c24ade
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-03 00:11:15 +0100

    create story model, and populate stories in dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,22 @@
+const Story = require('../models/Story')
+
 exports.login = (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 }
 
-exports.dashboard = (req, res) => {
-    res.render('dashboard')
+exports.dashboard = async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err);
+        res.render('error/500')
+    }
+
 }
 
