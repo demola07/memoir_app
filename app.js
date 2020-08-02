818845d8f74cfae700b1c58a8b0a9f21eab610c8
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-02 23:52:23 +0100

    store session in database

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv')
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const connectDB = require('./config/db')
 
@@ -31,6 +33,8 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
 // Passport middleware
