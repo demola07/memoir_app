33dd203a54fa6377879853ba9cc9d8eae01b2ec5
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-03 09:46:43 +0100

    implement edit story functionality

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 // Handlebars
-app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate }, defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate, editIcon }, defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 // Express sessions
@@ -46,6 +46,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 //  Static folder
 app.use(express.static(path.join(__dirname, 'public')))
