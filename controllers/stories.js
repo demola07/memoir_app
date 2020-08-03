6af782758d35090537390024e0f46daaac189b98
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-03 10:47:05 +0100

    clean up code and include proper error handling

--- controllers/stories.js
+++ controllers/stories.js
@@ -29,37 +29,49 @@ exports.getAllStories = async (req, res) => {
 }
 
 exports.editStory = async (req, res) => {
-    const story = await Story.findOne({ _id: req.params.id }).lean()
+    try {
+        const story = await Story.findOne({ _id: req.params.id }).lean()
 
-    if (!story) {
-        return res.render('error/404')
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', {
+                story
+            })
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500')
     }
 
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
-    } else {
-        res.render('stories/edit', {
-            story
-        })
-    }
 }
 
 exports.updateStory = async (req, res) => {
-    let story = await Story.findById(req.params.id).lean()
+    try {
+        let story = await Story.findById(req.params.id).lean()
 
-    if (!story) {
-        return res.render('error/404')
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/500')
     }
 
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
-    } else {
-        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true
-        })
-        res.redirect('/dashboard')
-    }
 }
 
 exports.deleteStory = async (req, res) => {
